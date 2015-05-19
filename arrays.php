804175df6e91f01f4133f665c7322235eb4e5dee
Kyle Morton <kyle@thekylemorton.com>
2015-05-18 17:14:45 -0700

    Finished Section 2 Practice

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $numberList = array(23,64,247,3523,'thing', '<h1>Hello</h1>');
+    
+echo $numberList[0];
+echo $numberList[1];
+echo $numberList[2];
+echo $numberList[3];
+echo $numberList[4];
+echo $numberList[5];
+
+//print_r($numberList);
+    ?>
+    
+    
+    
+</body>
+</html>
---
+++ associative_arrays.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$names = array("first_name" => "John", "last_name" => "Larisange");
+//print_r($names);
+
+
+?>
+</body>
+</html>
